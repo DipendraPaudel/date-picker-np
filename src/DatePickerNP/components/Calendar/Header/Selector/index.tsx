84e0937b28586a458e2cc8639f559b88420f7ce7
Dipendra Paudel <dipendra.paudel@codehimalaya.net>
2024-11-10 20:40:05 +0545

    min and max dates disable the arrow order arranged and scrolling also blocked if disabled

--- src/DatePickerNP/components/Calendar/Header/Selector/index.tsx
+++ src/DatePickerNP/components/Calendar/Header/Selector/index.tsx
@@ -29,8 +29,42 @@ const Selector = ({
   // store selected year in the store so that handleChange can be called when selecting the month
   const [selectedYear, setSelectedYear] = useState(year);
 
+  let minYear = INITIAL_YEAR_OF_CALENDAR;
+  let maxYear = FINAL_YEAR_OF_CALENDAR;
+  let minMonth = 1;
+  let maxMonth = 12;
+
+  const actualStartYear = startYear + INITIAL_YEAR_OF_CALENDAR;
+  const actualEndYear = startYear + 11 + INITIAL_YEAR_OF_CALENDAR;
+
+  // only change minYear if passed min value is valid
+  if (min && isValidNepaliDate(min)) {
+    const { year, month } = extractDateData(min);
+    minYear = +year;
+
+    if (selectedYear === +year) minMonth = +month;
+  }
+
+  // only change maxYear if passed max value is valid
+  if (max && isValidNepaliDate(max)) {
+    const { year, month } = extractDateData(max);
+    maxYear = +year;
+
+    if (selectedYear === +year) maxMonth = +month;
+  }
+
+  const isPrevArrowDisabled = actualStartYear <= minYear;
+  const isNextArrowDisabled = actualEndYear >= maxYear;
+
   // function to handle years selector options to be displayed on negative and positive units
   const handleStartYearChange = (deltaY: number) => {
+    // if prev or next arrow is disabled then disable the scrolling also to respective directions
+    if (
+      (deltaY < 0 && isPrevArrowDisabled) ||
+      (deltaY > 0 && isNextArrowDisabled)
+    )
+      return;
+
     setStartYear((startYear) => {
       const changeYear = startYear + (deltaY > 0 ? 12 : -12);
 
@@ -63,33 +97,6 @@ const Selector = ({
 
     // eslint-disable-next-line
   }, []);
-
-  let minYear = INITIAL_YEAR_OF_CALENDAR;
-  let maxYear = FINAL_YEAR_OF_CALENDAR;
-  let minMonth = 1;
-  let maxMonth = 12;
-
-  // only change minYear if passed min value is valid
-  if (min && isValidNepaliDate(min)) {
-    const { year, month } = extractDateData(min);
-    minYear = +year;
-
-    if (selectedYear === +year) minMonth = +month;
-  }
-
-  // only change maxYear if passed max value is valid
-  if (max && isValidNepaliDate(max)) {
-    const { year, month } = extractDateData(max);
-    maxYear = +year;
-
-    if (selectedYear === +year) maxMonth = +month;
-  }
-
-  const actualStartYear = startYear + INITIAL_YEAR_OF_CALENDAR;
-  const actualEndYear = startYear + 11 + INITIAL_YEAR_OF_CALENDAR;
-
-  const isPrevArrowDisabled = actualStartYear <= minYear;
-  const isNextArrowDisabled = actualEndYear >= maxYear;
 
   return (
     <div className="date-picker-selector-container">
